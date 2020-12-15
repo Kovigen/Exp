cf850726f602706d13782d9179f8704fdbdb3710
KoViGe <dead1inside1anubis@gmail.com>
2020-12-15 13:44:52 +0200

    Задание=)

---
+++ 10.12.2020.py
@@ -0,0 +1,6 @@
+x = int(input())
+k = int(input())
+a = k // x
+b = k % x
+print(k)
+print(x)
---
+++ 30.11.2020.py
@@ -0,0 +1,7 @@
+def tickets(people)
+   if  vector int[25, 25, 50, 100, 25] = yes
+       else vector int[25,50,100,50] = no
+x = "Mike"
+
+
+    return "?"
