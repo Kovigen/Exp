9e49bbc4c6523aff6ea72311d69155fb14744523
KoViGe <dead1inside1anubis@gmail.com>
2020-12-29 23:51:41 +0200

    '29.12.2020'

---
+++ 29.12.2020.w.py
@@ -0,0 +1,11 @@
+import math
+a = float(input())
+b = float(input())
+c = (a + b) / 2
+print(c)
+d = math.sqrt(a * b)
+print(d)
+f = (2 * a * b) / (a + b)
+print(f)
+g = math.sqrt(((a)**2 + (b)**2) / 2)
+print(g)
---
+++ 29.12.2020a.py
@@ -0,0 +1,6 @@
+import math
+r = float(input())
+pi = math.pi
+s = (pi * (r)**2)
+print(s)
+a = 2 * pi * r
